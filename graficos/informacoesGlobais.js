aad0530c2e5c02bc876210d21308aaa0ac96cccf
Guilherme Alessi <71829266+Guilherme-Alessi@users.noreply.github.com>
2024-10-04 08:25:07 -0300

    dg

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,5 +1,3 @@
-const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
-
 async function visualizarInformacoesGlobais() {
 
         const res = await fetch(url)
