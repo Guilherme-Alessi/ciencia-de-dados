107f104cf5dd5da2c08175f69fe13b52f87752cd
Guilherme Alessi <71829266+Guilherme-Alessi@users.noreply.github.com>
2024-10-18 08:26:01 -0300

    hk

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -8,6 +8,13 @@ async function visualizarInformacoesGlobais() {
 
         console.log(dados);
 
+        const paragrafo = document.createElement('p')
+        paragrafo.classList.add('graicos-container_texto')
+        paragrafo.innerHTML = 'você sabia que o muno tem ${dados.total_pessoas_mundo} de pessoas e aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.'
+
+        const container = document.getElementById('graicos-container')
+        container.appendChild(paragrafo)
+
     }
 
     visualizarInformacoesGlobais()
