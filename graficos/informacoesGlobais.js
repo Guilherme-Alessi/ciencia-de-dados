01fefe976724adb51011eaf8449148cef367e90d
Guilherme Alessi <71829266+Guilherme-Alessi@users.noreply.github.com>
2024-10-18 08:28:28 -0300

    ads

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -10,7 +10,7 @@ async function visualizarInformacoesGlobais() {
 
         const paragrafo = document.createElement('p')
         paragrafo.classList.add('graicos-container_texto')
-        paragrafo.innerHTML = 'você sabia que o muno tem ${dados.total_pessoas_mundo} de pessoas e aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.'
+        paragrafo.innerHTML = 'você sabia que o muno tem <span>${dados.total_pessoas_mundo}</span> de pessoas e aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.'
 
         const container = document.getElementById('graicos-container')
         container.appendChild(paragrafo)
